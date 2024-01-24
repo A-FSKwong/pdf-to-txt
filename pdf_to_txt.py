9d563e098537b0f5590a9ed14ac7978942a647fc
A-FSKwong <157028826+A-FSKwong@users.noreply.github.com>
2024-01-24 20:18:27 +0800

    Update pdf_to_txt.py
    added Powershell to shows "Total pages converted"

--- pdf_to_txt.py
+++ pdf_to_txt.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # importing required modules
+
 try:
     import PyPDF2
 except ImportError:
@@ -7,18 +8,24 @@ except ImportError:
     os.system('pip install PyPDF2')
     import PyPDF2
 
+
 def extract_text_from_pdf(pdf_filename, txt_filename):
     # creating a pdf reader object 
     reader = PyPDF2.PdfReader(pdf_filename) 
 
+
     # printing number of pages in pdf file 
-    print(len(reader.pages)) 
+    print(f"Total pages in the PDF: {len(reader.pages)}") 
+
 
     # extracting text from all pages and saving to txt file
     with open(txt_filename, 'w', encoding='utf-8') as f:  # Open the file with UTF-8 encoding
-        for page in reader.pages:
+
+        for i, page in enumerate(reader.pages):
             text = page.extract_text() 
             f.write(text + '\n')
+            print(f"Total pages converted: {i+1}", end="\r")
+
 
 if __name__ == "__main__":
     pdf_filename = input("Please enter the path to the PDF file: ")
@@ -31,5 +38,8 @@ if __name__ == "__main__":
     else:
         # Migrating starts here....
         extract_text_from_pdf(pdf_filename, txt_filename)
+
+
+    print("\nAll Done.")
 
 
